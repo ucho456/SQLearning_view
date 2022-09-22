d1ef0efa1fcf2f9b7e267c538a05d9cdff2e8ec9
ucho456 <watapqxyz@gmail.com>
2022-09-22 16:50:30 +0900

    不要ファイル削除 + Linter設定

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,40 +1,24 @@
-<script setup lang="ts">
-defineProps<{
-  msg: string
-}>()
-</script>
-
 <template>
-  <div class="greetings">
-    <h1 class="green">{{ msg }}</h1>
-    <h3>
-      You’ve successfully created a project with
-      <a href="https://vitejs.dev/" target="_blank" rel="noopener">Vite</a> +
-      <a href="https://vuejs.org/" target="_blank" rel="noopener">Vue 3</a>. What's next?
-    </h3>
-  </div>
+  <h1>{{ message }}</h1>
 </template>
 
-<style scoped>
-h1 {
-  font-weight: 500;
-  font-size: 2.6rem;
-  top: -10px;
-}
+<script lang="ts">
+import { defineComponent, ref } from 'vue'
 
-h3 {
-  font-size: 1.2rem;
-}
+export default defineComponent({
+  props: {
+    msg: {
+      type: String,
+      required: true
+    }
+  },
+  setup(props) {
+    const count = ref(0)
 
-.greetings h1,
-.greetings h3 {
-  text-align: center;
-}
-
-@media (min-width: 1024px) {
-  .greetings h1,
-  .greetings h3 {
-    text-align: left;
+    return {
+      count,
+      message: props.msg
+    }
   }
-}
-</style>
+})
+</script>
